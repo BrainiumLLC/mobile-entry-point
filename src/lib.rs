91a09d3f40c3429446f2f0da7eab44aceef902cf
Francesca Lovebloom <francesca@brainiumstudios.com>
2020-05-05 13:21:20 -0700

    Use ndk-glue

--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,8 @@ pub fn mobile_entry_point(
         fn stop_unwind<F: FnOnce() -> T, T>(f: F) -> T {
             match std::panic::catch_unwind(std::panic::AssertUnwindSafe(f)) {
                 Ok(t) => t,
-                Err(e) => {
-                    let msg = format!("Attempt to Unwind out of `rust` with err: {:?}", e);
-                    #[cfg(not(target_os = "android"))]
-                    eprintln!("{}", msg);
-                    #[cfg(target_os = "android")]
-                    old_glue::write_log(&msg);
+                Err(err) => {
+                    eprintln!("attempt to unwind out of `rust` with err: {:?}", err);
                     std::process::abort()
                 }
             }
@@ -34,26 +30,7 @@ pub fn mobile_entry_point(
         }
 
         #[cfg(target_os = "android")]
-        #[no_mangle]
-        #[inline(never)]
-        pub extern "C" fn android_main(app: *mut std::os::raw::c_void) {
-            stop_unwind(|| {
-                old_glue::android_main2(app as *mut _, move |_, _| {
-                    start_app();
-                });
-            })
-        }
-
-        #[cfg(target_os = "android")]
-        #[no_mangle]
-        #[inline(never)]
-        pub unsafe extern "C" fn native_activity_on_create(
-            activity: *mut std::os::raw::c_void,
-            saved_state: *mut std::os::raw::c_void,
-            saved_state_size: usize,
-        ) {
-            old_glue::ANativeActivity_onCreate(activity, saved_state, saved_state_size);
-        }
+        ndk_glue::ndk_glue!(start_app);
     };
     //panic!("{}", expanded.to_string());
     expanded.into()
