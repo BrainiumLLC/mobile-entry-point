2937d34104bd950a5f4c812796d31a649d51f35c
Francesca Lovebloom <francesca@brainiumstudios.com>
2020-07-29 14:14:07 -0700

    Use new ndk-glue macro

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,7 @@ pub fn mobile_entry_point(
             }
         }
 
+        #[cfg_attr(target_os = "android", ndk_glue::main(backtrace))]
         fn _start_app() {
             #func
             stop_unwind(|| #name());
@@ -32,9 +33,6 @@ pub fn mobile_entry_point(
         pub extern "C" fn start_app() {
             _start_app()
         }
-
-        #[cfg(target_os = "android")]
-        ndk_glue::ndk_glue!(_start_app);
     };
     //panic!("{}", expanded.to_string());
     expanded.into()
